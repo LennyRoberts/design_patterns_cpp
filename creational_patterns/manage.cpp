2c13212019aa65015b544ef142e8643df45a68c1
LennyRoberts <2427541459@qq.com>
2022-03-06 21:30:25 +0800

    modify

---
+++ creational_patterns/manage.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "factory_method.h"
+#include "abstract_factory.h"
+#include "builder.h"
+#include "prototype.h"
+#include "singleton.h"
+
+using namespace std;
+
+int main(int argc, char *argv[ ])
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
